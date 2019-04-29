b9d570431b5407428a33cd06c12367967437125f
Stuart Hillam <sjhillam@gmail.com>
2019-04-28 23:18:20 -0600

    routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+// Views
 import Home from './views/Home.vue'
+import Search from './views/Search.vue'
+import Browse from './views/Browse.vue'
 
 Vue.use(Router)
 
@@ -8,18 +12,8 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: '/', component: Home }, // Route 1
+    { path: '/search', component: Search }, // Route 2
+    { path: '/browse', component: Browse } // Route 3
   ]
 })
